5c38f3ec7fd3e938e84c8f57ba1fa0103d609992
hollocherDale <hollocher@gmail.com>
2014-05-08 12:40:25 -0700

    begin vote from scatch

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,9 +1,7 @@
 class Vote < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
+  validates :user_id, uniqueness: { scope: :post_id }
 
-  validates_uniqueness_of :post_id, scope: :user_id
-#  validates_inclusion_of :value, in: [1, -1]
-#  validate :ensure_not_author
 
 end
