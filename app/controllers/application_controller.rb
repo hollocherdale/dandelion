63850750d0772831293db08c6e849e0eaf542158
hollocherdale <hollocher@gmail.com>
2014-07-03 12:10:51 -0700

    name now works in signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(User)
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
     else
       super
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
   end
 end
