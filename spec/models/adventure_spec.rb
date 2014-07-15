9901bb9ac8703b1eca486d2836e529e7ac0dca16
hollocherdale <hollocher@gmail.com>
2014-07-15 12:44:51 -0700

    fixed pseudo passing tests

--- spec/models/adventure_spec.rb
+++ spec/models/adventure_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 describe Adventure do
+  let(:user) { FactoryGirl.create(:user) }
+  before { @adventure = user.adventures.build(story: 'story', choice: 'choice') }
+  subject! { @adventure }
+
   it { should respond_to(:story) }
   it { should respond_to(:choice) }
   it { should respond_to(:user) }
-
-  # I don't know why I need to create a user or adventure
-  
-  # let(:user) { FactoryGirl.create(:user) }
-  # before { @adventure = user.adventures.build(story: 'story', choice: 'choice') }
-  # subject { @adventure }
 
   it 'has state' do
     expect(subject.state).to eq('pending')
   end
 
-  describe 'how the state can be changed' do
-    around(:each) { subject.publish }
+  describe 'state can be changed' do
+    before(:each) { subject.publish }
 
     context 'when published' do
       it 'should be accepting_submissions' do
@@ -25,14 +23,14 @@ describe Adventure do
     end
 
     context 'when populated' do
-      around(:each) { subject.populate }
+      before(:each) { subject.populate }
 
       it 'should be accepting_submissions' do
         expect(subject.state).to eq('populated')
       end   
 
       context 'when unpopulated' do
-        around { subject.unpopulate }
+        before { subject.unpopulate }
 
         it 'should be accepting_submissions' do
           expect(subject.state).to eq('accepting_submissions')
@@ -40,7 +38,7 @@ describe Adventure do
       end
 
       context 'when upvoted' do
-        around { subject.upvoted }
+        before { subject.upvoted }
 
         it 'should be accepting_submissions' do
           expect(subject.state).to eq('popular')
@@ -48,15 +46,15 @@ describe Adventure do
       end
 
       context 'when closed' do
-        around { subject.close }
+        before { subject.close }
 
         it 'should be accepting_submissions' do
           expect(subject.state).to eq('closed')
         end
 
         context 'when popular' do
-          around { subject.upvote }
-          around { subject.close }
+          before { subject.upvoted }
+          before { subject.close }
 
           it 'should be accepting_submissions' do
             expect(subject.state).to eq('closed')
