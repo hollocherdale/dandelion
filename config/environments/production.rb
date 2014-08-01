ce4a05a05e465cd12cd4dc9749497cf19ed53be8
hollocherdale <hollocher@gmail.com>
2014-08-01 14:30:57 -0700

    refactor confg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,6 @@ Dandelion::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['S3_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
-    }
+    :s3_host_name => 's3-us-west-2.amazonaws.com'
   }
-  Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
 end
