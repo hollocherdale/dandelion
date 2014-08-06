6c2ec3f65729e56ee1a301c5b8fab5cc9dab08ab
hollocherdale <hollocher@gmail.com>
2014-08-06 16:19:11 -0700

    qf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Dandelion::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME_DEVELOPMENT'],
+      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['S3_ACCESS_KEY_ID'],
       :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
     }
