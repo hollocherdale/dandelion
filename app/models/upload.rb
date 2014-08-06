6f83e5e975203d0273d58788feae7f4403206ea6
hollocherdale <hollocher@gmail.com>
2014-08-06 16:15:01 -0700

    qf

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,14 +3,6 @@ class Upload < ActiveRecord::Base
   has_attached_file :image, styles: {
                     large:    '1500x1500>',
                     thumb:    '100x100>'
-                    },
-                    :storage => :s3,
-                    :bucket => ENV['S3_BUCKET_NAME_DEVELOPMENT'],
-                    :s3_credentials => {
-                             :access_key_id => ENV['S3_ACCESS_KEY_ID'],
-                             :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
-                         },
-                   :s3_permissions => 'private',
-                   :url => ":s3_domain_url"
+                    }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,6 @@
 Paperclip::Attachment.default_options.merge!({
                     :storage => :s3,
-                    :bucket => ENV['S3_BUCKET_NAME_DEVELOPMENT'],
+                    :bucket => ENV['S3_BUCKET_NAME'],
                     :s3_credentials => {
                              :access_key_id => ENV['S3_ACCESS_KEY_ID'],
                              :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
