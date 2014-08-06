45312129595574ad6d5f07451d73be4a4d5ca5a1
hollocherdale <hollocher@gmail.com>
2014-08-06 15:54:35 -0700

    add bucket options to upload model for heroku

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,6 +3,8 @@ class Upload < ActiveRecord::Base
   has_attached_file :image, styles: {
                     large:    '1500x1500>',
                     thumb:    '100x100>'
-                    }
+                    },
+                    :storage => :s3,
+                    :bucket => ENV['S3_BUCKET_NAME_DEVELOPMENT']
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
