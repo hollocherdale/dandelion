084f1e91f29947ea390cec9cb9cbf94e3825e71a
hollocherdale <hollocher@gmail.com>
2014-08-06 15:59:27 -0700

    update upload hash

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -5,6 +5,12 @@ class Upload < ActiveRecord::Base
                     thumb:    '100x100>'
                     },
                     :storage => :s3,
-                    :bucket => ENV['S3_BUCKET_NAME_DEVELOPMENT']
+                    :bucket => ENV['S3_BUCKET_NAME_DEVELOPMENT'],
+                    :s3_credentials => {
+                             :access_key_id => ENV['S3_ACCESS_KEY_ID'],
+                             :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
+                         },
+                   :s3_permissions => 'private',
+                   :url => ":s3_domain_url"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
