2db082f3fe0ae55d6b07e9be9a1a512741edc4ab
hollocherdale <hollocher@gmail.com>
2014-08-06 14:40:39 -0700

    qe

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,9 +1,8 @@
 class Upload < ActiveRecord::Base
-  has_attached_file :image, styles: {
-    large:    '1500x1500>',
-    thumb:    '100x100>'
-  }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
   belongs_to :adventure
+  has_attached_file :image, styles: {
+                    large:    '1500x1500>',
+                    thumb:    '100x100>'
+                    }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
