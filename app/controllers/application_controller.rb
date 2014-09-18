4ff3e4d0487a8b6a2315c89985f054ce98ee27a3
hollocherdale <hollocher@gmail.com>
2014-09-18 15:42:23 -0700

    now redirects to where user last visited before signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource_or_scope)
-    if resource_or_scope.is_a?(User)
-      root_url
-    else
-      super
-    end
+  after_filter :store_location
+
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
   end
 
   protected
