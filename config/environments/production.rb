b8d0f7a6e638c99dbf63db3ecf66f61e72d6089b
hollocherdale <hollocher@gmail.com>
2014-09-21 14:27:06 -0700

    quick change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,6 @@ Dandelion::Application.configure do
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: dandelion.herokuapp.com }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com', 
     port: 587,
