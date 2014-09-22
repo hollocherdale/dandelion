cb90a74028415a02ed68d3d8aa9dc97c8891545e
hollocherdale <hollocher@gmail.com>
2014-09-21 21:37:33 -0700

    getting undefined meth skip confirm

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,6 @@ class User < ActiveRecord::Base
           email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
           password: Devise.friendly_token[0,20]
         )
-        user.skip_confirmation!
         user.save!
       end
     end
