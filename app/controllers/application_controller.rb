248e1c4c5e8a36697d73bd7cde8e32471ee2e810
hollocherdale <hollocher@gmail.com>
2014-09-22 15:50:47 -0700

    redirect now works with omniauth and normal signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  after_filter :store_location
-
-  def store_location
-    # store last url as long as it isn't a /users path
-    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
-  end
-
   def after_sign_in_path_for(resource)
-    session[:previous_url] || root_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
   end
 
   protected
